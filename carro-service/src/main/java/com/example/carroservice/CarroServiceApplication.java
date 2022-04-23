23103cdca5976af2e94cf34311de3d04eb862ebd
Wilson <whernandez.extreme@gmail.com>
2022-04-23 11:42:08 -0500

    eureka

--- carro-service/src/main/java/com/example/carroservice/CarroServiceApplication.java
+++ carro-service/src/main/java/com/example/carroservice/CarroServiceApplication.java
@@ -3,9 +3,8 @@ package com.example.carroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
-@SpringBootApplication
 @EnableEurekaClient
+@SpringBootApplication
 public class CarroServiceApplication {
 
     public static void main(String[] args) {
