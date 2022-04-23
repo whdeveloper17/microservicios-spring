61e0627803a6cc6cf797a957f28a897f4d6faaf8
Wilson <whernandez.extreme@gmail.com>
2022-04-22 21:20:08 -0500

    config-server

--- carro-service/src/main/java/com/example/carroservice/CarroServiceApplication.java
+++ carro-service/src/main/java/com/example/carroservice/CarroServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.carroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Enablecon
 @SpringBootApplication
 public class CarroServiceApplication {
 
